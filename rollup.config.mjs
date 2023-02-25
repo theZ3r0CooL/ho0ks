da4ad8e359cb88cb7826c043f423833098f44efb
Z3r0CooL <103323173+theZ3r0CooL@users.noreply.github.com>
2023-02-24 21:43:58 -0500

    - Adding some more files to project and example. - Removes `.env`. - Add /dist to `.gitignore` since action will be used to build dist on push trigger. - Updated dependencies, removed unnecessary dependencies. - Added override for `@svgr/webpack` to satisfy nth-check complexity ond dependent.

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -4,7 +4,7 @@ import resolve from '@rollup/plugin-node-resolve'
 import external from 'rollup-plugin-peer-deps-external'
 import url from '@rollup/plugin-url'
 
-import pkg from './package.json'
+import pkg from './package.json' assert {type: 'json'}
 
 export default {
   input: 'src/index.tsx',
